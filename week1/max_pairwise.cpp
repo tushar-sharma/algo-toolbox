92a6aeaf13b970130b7db8c97c5eb6672fff3f27
Tushar Sharma <tushar.sharma1729@gmail.com>
2017-04-02 00:46:36 -0400

    week 1 updated

--- week1/max_pairwise.cpp
+++ week1/max_pairwise.cpp
@@ -28,6 +28,7 @@ long long  MaxPairWiseProduct_naive(const vector<int>& numbers) {
  * Output: Number of type long long
  */
 long long MaxPairWiseProductFast(const vector<int>& numbers) {
+
     int max_index1 = -1;
     for(int i = 0; i < numbers.size(); ++i) {
         if((max_index1 == -1) || (numbers[i] > numbers[max_index1])) {
@@ -37,24 +38,14 @@ long long MaxPairWiseProductFast(const vector<int>& numbers) {
 
     int max_index2 = -1;
     for (int j = 0; j < numbers.size(); j++) {
-        if (((numbers[j] != numbers[max_index1]) && (max_index2 == -1)) || ((numbers[j] > numbers[max_index2]) && (numbers[j] != numbers[max_index1])))
+        if (((j != max_index1) && (max_index2 == -1)) || ((numbers[j] > numbers[max_index2]) && (j != max_index1)))
             max_index2 = j;
     }
-    return  (((long long) numbers[max_index1]) * ((long long) numbers[max_index2]));
+
+    return  (long long) numbers[max_index1] * (long long) numbers[max_index2];
 }
 
-int main(int argc, char **argv) {
-    /*
-	int n;
-    cin>>n;
-
-    vector<int> numbers(n);
-
-    for (int i = 0; i < n; ++i) {
-        cin >> numbers[i];
-    }
-    */
-
+void tests() {
     assert(MaxPairWiseProduct_naive(vector<int> {2, 3, 4, 5}) == 20);
     assert(MaxPairWiseProduct_naive(vector<int> {9, 2, 7, 4, 21}) == 189);
     assert(MaxPairWiseProduct_naive(vector<int> {3, 8}) == 24);
@@ -68,6 +59,23 @@ int main(int argc, char **argv) {
     assert(MaxPairWiseProductFast(vector<int> {3, 8}) == 24);
     assert(MaxPairWiseProductFast(vector<int> {90000, 100000}) == 9000000000);
     assert(MaxPairWiseProductFast(vector<int> (1000000, 0)) == 0);
+}
+
+int main(int argc, char **argv) {
+
+    int n;
+    cin>>n;
+
+    vector<int> numbers(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> numbers[i];
+    }
+
+    long long result = MaxPairWiseProductFast(numbers);
+    cout<<result<< "\n";
+
+    //tests();
 
     return 0;
 }
