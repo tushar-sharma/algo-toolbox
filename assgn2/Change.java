ff5f16f94919326721b417d9de274e8dd0bc0549
Tushar Sharma <tushar.sharma1729@gmail.com>
2017-04-10 00:51:46 -0400

    typo in assgn fixed

--- assgn2/Change.java
+++ assgn2/Change.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 /*
- * Chaning Money
+ * Changing Money
  * The goal in this problem is to find the minimum number of coins needed to change the input value
  * (an integer) into coins with denominations 1, 5, and 10.
  */
